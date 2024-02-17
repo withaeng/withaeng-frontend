837049588af74a74de219cccf5671a4e21dc5729
sodam park <73267057+dev-damdam@users.noreply.github.com>
2024-02-17 14:08:13 +0900

    refator: components, styles 폴더 src 하위 폴더로 이동 (#5)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Header from "./components/header/Header";
+import Header from "../components/header/Header";
 
 const pretendard = localFont({
   src: "../../public/assets/fonts/PretendardVariable.woff2",
