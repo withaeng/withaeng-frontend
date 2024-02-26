2e59190e5615e352d02acd75f49ec3e4190838e2
parksol <thfdl0317@naver.com>
2024-02-26 14:09:49 +0900

    fix : WhIcon 이미지가 있는 경우, 없는 경우관련 코드 수정

--- src/app/sample/ui-elements/WhIcon.tsx
+++ src/app/sample/ui-elements/WhIcon.tsx
@@ -2,23 +2,22 @@ import React from 'react';
 import Image from 'next/image';
 
 export interface IconProps {
-  src: boolean;
+  src: 'string | StaticImport';
 }
 
-const WhIcon: React.FC<IconProps> = ({ src }) => {
-  return (
-    // keep going
-    <>
-      {src ? null : (
-        <Image
-          width={20}
-          height={20}
-          src='/assets/icons/sample-icon.svg'
-          alt='sample-icon'
-        />
-      )}
-    </>
-  );
-};
+const WhIcon: React.FC<IconProps> = ({ src }) => (
+  <div>
+    {src ? (
+      <Image width={20} height={20} src={src} alt='sample-icon' />
+    ) : (
+      <Image
+        width={20}
+        height={20}
+        src='/assets/icons/sample-icon.svg'
+        alt='sample-icon'
+      />
+    )}
+  </div>
+);
 
 export default WhIcon;
