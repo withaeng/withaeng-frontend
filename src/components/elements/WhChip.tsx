26eb5c865427f81c52a8fc19944af86318bdef0f
parksol <thfdl0317@naver.com>
2024-03-05 23:04:54 +0900

    add : ui-elements chip경로 생성

--- src/components/elements/WhChip.tsx
+++ src/components/elements/WhChip.tsx
@@ -3,7 +3,7 @@
 import React, { ReactNode } from 'react';
 
 const defaultChipCss =
-  'py-2 px-4 rounded-[20px] hover:bg-nutral-white-02 disabled:opacity-40 bg-nutral-white-01 disabled:bg-nutral-white-01  disabled:text-nutral-black-03 disabled:border-nutral-white-03 focus:bg-primary-eexLight focus:text-primary-pressing focus:border-primary-main border border-nutral-white-03 transition text-subtitle-02 flex items-center justify-center gap-2.5 text-nutral-black-03';
+  'py-2 px-4 rounded-[20px] hover:bg-nutral-white-02 disabled:opacity-40 bg-nutral-white-01 disabled:bg-nutral-white-01  text-nutral-black-03 disabled:border-nutral-white-03 focus:bg-primary-eexLight focus:text-primary-pressing focus:border-primary-main border border-nutral-white-03 transition text-subtitle-02 flex items-center justify-center gap-2.5 text-nutral-black-03';
 
 interface ChipProps {
   children: ReactNode;
