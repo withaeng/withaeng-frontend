c2d5c7f612d1a9a2c4492d5b8ba96b6f99ef018e
신수진 <shinsj5037@gmail.com>
2024-03-09 00:28:36 +0900

    fix, build: svg 파일을 react component로 불러오도록 변경

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
-import Image from 'next/image';
-import menuIcon from '../../../public/assets/icons/hamburger-menu.svg';
+import { ReactComponent as MenuIcon } from '/public/assets/icons/hamburger-menu.svg';
 // TODO: ProfilIconGray01 제외하고 그 외 프로필 아이콘은 제거 예정 > 서버에서 전달 받음
 import {
   ProfilIconGray01,
@@ -23,6 +22,7 @@ const menuLinkCss =
 
 // TODO: 프로필 아이콘은 제거 예정 > 서버에서 전달 받음
 const profileIcons = [
+  ProfilIconGray01,
   ProfilIconBlue01,
   ProfilIconGreen01,
   ProfilIconGreen02,
@@ -36,7 +36,7 @@ const profileIcons = [
 
 export default function Header() {
   // TODO: GNG profile icon 서버에서 받아서 표시
-  const randomProfileIcon = profileIcons[Math.floor(Math.random() * 9)];
+  const RandomProfileIcon = profileIcons[Math.floor(Math.random() * 10)];
   return (
     <header className='flex items-center justify-center w-full px-5 py-3 border-b border-nutral-white-03'>
       <Container>
@@ -59,20 +59,11 @@ export default function Header() {
 
         {/* profile menu */}
         <div className='flex items-center gap-3 px-4 py-3 border rounded-full border-nutral-white-03'>
-          <Image src={menuIcon} alt='메뉴' />
+          <MenuIcon />
           <div className='relative w-8 h-8'>
             {/* TODO: 서버에서 url 전달받아 표시 예정 > width, height 값 지정 필요 */}
             {/* mouse hover 이벤트 발생 시 프로필 아이콘 투명도를 이용해서 변경 */}
-            <Image
-              src={ProfilIconGray01}
-              alt='프로필 이미지'
-              className='absolute w-full h-full opacity-100 hover:opacity-0'
-            />
-            <Image
-              src={randomProfileIcon}
-              alt='프로필 이미지'
-              className='absolute w-full h-full opacity-0 hover:opacity-100'
-            />
+            <RandomProfileIcon />
           </div>
         </div>
       </Container>
--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -1,11 +1,10 @@
-import Image from 'next/image';
 import Link from 'next/link';
-import LogoImage from '../../../public/assets/images/withaeng-logo-transparent.svg';
+import { ReactComponent as LogoImage } from '../../../public/assets/images/withaeng-logo-transparent.svg';
 
 export default function Logo() {
   return (
     <Link className='h-fit' href='/'>
-      <Image src={LogoImage} alt='같이행 로고' priority />
+      <LogoImage />
     </Link>
   );
 }
