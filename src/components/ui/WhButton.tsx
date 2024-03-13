62573d150e43c9517a2197624963ebfe06159e49
박 SOL <63778599+SOL-hub@users.noreply.github.com>
2024-03-14 00:12:17 +0900

    feat: WhButton 구현 (#12)
    * feat : 공통 버튼 컴포넌트 작업 중, nutral-white-04 색상 수정
    
    * add : WhButton 컴포넌트 사이즈 설정 추가
    
    * fix : 버튼 컴포넌트 사이즈관련 코드 수정, ts 버전,  tsconfig 수정
    add : lint rules 추가, WhIcon 컴포넌트 생성 및 코드 추가
    
    * fix : WhIcon 이미지가 있는 경우, 없는 경우관련 코드 수정
    
    * add : WhButton 클래스명 추가, ts버전 수정 등
    
    * fix : 버튼에 아이콘  true 경우 사이 사이즈 변경
    
    * fix : 클래스네이밍 수정, md 추가
    
    * move : 공통 컴포넌트들 /src/components/ui로 이동
    
    * fix : WhIcon 이미지관련 props 추가 및 수정,  WhButton 함수 표현식으로 변경
    
    * fix :  defaultProps지정, ui-element에서 button관련 링크 추가
    
    * fix : 버튼 스타일관련 피드백 반영, WhIcon 컴포넌트 안  isIcon 제거
    
    * fix : WhIcon 제거, onlyIcon관련 코드 제거 및 수정
    
    * feat: defaultProps deprecate되어 수정

---
+++ src/components/ui/WhButton.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import React, { ReactNode } from 'react';
+
+const defaultBtnCss =
+  'rounded-sm disabled:opacity-40 px-5 border border-primary-main transition text-subtitle-02 flex items-center justify-center gap-2.5 text-nutral-white-01';
+
+const outLineBtnCss =
+  'hover:bg-nutral-white-02 text-primary-main bg-nutral-white-01 disabled:text-nutral-white-03 disabled:border-nutral-white-04';
+
+const fillBtnCss = 'bg-primary-main hover:bg-primary-pressing';
+
+let btnSizeCss = '';
+
+interface ButtonProps {
+  children: ReactNode;
+  outLine?: boolean;
+  disabled?: boolean;
+  size: 'lg' | 'md' | 'sm';
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+export default function WhButton({
+  children,
+  disabled = false,
+  outLine = false,
+  size = 'md',
+  onClick,
+}: ButtonProps) {
+  if (size === 'lg') {
+    btnSizeCss = 'py-3 max-h-11';
+  } else if (size === 'md') {
+    btnSizeCss = 'py-2 max-h-9';
+  } else if (size === 'sm') {
+    btnSizeCss = 'py-1 max-h-7';
+  }
+
+  return (
+    <button
+      type='button'
+      disabled={disabled}
+      onClick={onClick}
+      className={`
+      ${defaultBtnCss}
+      ${btnSizeCss}
+      ${outLine ? `${outLineBtnCss}` : `${fillBtnCss}`}
+    `}
+    >
+      {children}
+    </button>
+  );
+}
