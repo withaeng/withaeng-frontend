77c997c64263b1113520ff2905f329e7c5a35dc7
parksol <thfdl0317@naver.com>
2024-03-31 07:52:24 +0900

    fix : 셀렉트 부분 수정

--- src/app/sample/ui-elements/chip/page.tsx
+++ src/app/sample/ui-elements/chip/page.tsx
@@ -13,19 +13,18 @@ export default function Page() {
   ];
 
   function handleChip(value: string) {
-    // TODO 셀렉트 버그 수정 필요
     if (!chipList.includes(value)) {
-      chipList.push(value);
-      setChipList([...chipList]);
+      setChipList([...chipList, value]);
     } else {
       const index = chipList.findIndex((el) => el === value);
-      const temp = chipList.slice(index, 1)
-      setChipList([...temp]);
+      const temp = [...chipList];
+      temp.splice(index, 1);
+      setChipList(temp);
     }
   }
 
   function isIncludes(value: string): boolean {
-    return chipList.includes(value) ? true : false;
+    return chipList.includes(value);
   }
 
   return (
@@ -47,7 +46,7 @@ export default function Page() {
               <WhChip
                 checked={isIncludes(item.title)}
                 value={item.title}
-                onClick={handleChip}
+                onClick={() => handleChip(item.title)}
               >
                 {item.title}
               </WhChip>
--- src/components/elements/WhChip.tsx
+++ src/components/elements/WhChip.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { ReactNode, useState } from 'react';
+import React, { ReactNode } from 'react';
 
 const defaultChipCss =
   'py-2 px-4 rounded-[20px] disabled:opacity-40 bg-nutral-white-01 disabled:bg-nutral-white-01  text-nutral-black-03 disabled:border-nutral-white-03 border border-nutral-white-03 transition text-subtitle-02 flex items-center justify-center gap-2.5 text-nutral-black-03';
@@ -23,7 +23,6 @@ export default function WhChip({
   checked = false,
   onClick = () => {}, // set default function
 }: ChipProps) {
-
   return (
     <button
       type='button'
