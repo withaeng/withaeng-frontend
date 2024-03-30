e03c10fd74d63a99f84644afee2b62a684ae51a4
dev-damdam <dev.sodam@gmail.com>
2024-03-30 11:38:13 +0900

    feat: 양방향 방식으로 변경

--- src/app/sample/ui-elements/chip/page.tsx
+++ src/app/sample/ui-elements/chip/page.tsx
@@ -1,38 +1,60 @@
 'use client';
 
 import WhChip from '@/components/elements/WhChip';
-
+import { useState } from 'react';
 
 export default function Page() {
+  const [chipList, setChipList] = useState<string[]>([]);
+
   const itemList = [
     { id: 0, title: '태그내용' },
     { id: 1, title: '태그내용2' },
     { id: 2, title: '태그내용3' },
   ];
 
+  function handleChip(value: string) {
+    // TODO 셀렉트 버그 수정 필요
+    if (!chipList.includes(value)) {
+      chipList.push(value);
+      setChipList([...chipList]);
+    } else {
+      const index = chipList.findIndex((el) => el === value);
+      const temp = chipList.slice(index, 1)
+      setChipList([...temp]);
+    }
+  }
+
+  function isIncludes(value: string): boolean {
+    return chipList.includes(value) ? true : false;
+  }
+
   return (
     <div className='flex m-10 gap-8'>
-    <div>
-      <p className='mb-5'>👉 default</p>
-      <WhChip >태그 내용</WhChip>
-    </div>
-    <div>
-      <p className='mb-5'>👉 disabled </p>
-      <WhChip  disabled>태그 내용</WhChip>
-    </div>
+      <div>
+        <p className='mb-5'>👉 default</p>
+        <WhChip>태그 내용</WhChip>
+      </div>
+      <div>
+        <p className='mb-5'>👉 disabled </p>
+        <WhChip disabled>태그 내용</WhChip>
+      </div>
 
-    <div>
-      <p className='mb-5'>👉 여러개일 경우</p>
-    <div className='flex gap-3'>
-
-      {itemList.map((item) => (
-        <div key={item.id}>
-          <WhChip  >{item.title}</WhChip>
+      <div>
+        <p className='mb-5'>👉 여러개일 경우</p>
+        <div className='flex gap-3'>
+          {itemList.map((item) => (
+            <div key={item.id}>
+              <WhChip
+                checked={isIncludes(item.title)}
+                value={item.title}
+                onClick={handleChip}
+              >
+                {item.title}
+              </WhChip>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </div>
-
-    </div>
-  </div>
   );
 }
--- src/components/elements/WhChip.tsx
+++ src/components/elements/WhChip.tsx
@@ -5,35 +5,34 @@ import React, { ReactNode, useState } from 'react';
 const defaultChipCss =
   'py-2 px-4 rounded-[20px] disabled:opacity-40 bg-nutral-white-01 disabled:bg-nutral-white-01  text-nutral-black-03 disabled:border-nutral-white-03 border border-nutral-white-03 transition text-subtitle-02 flex items-center justify-center gap-2.5 text-nutral-black-03';
 
-const isSelectedCss = 'bg-primary-eexLight text-primary-pressing border-primary-main';
+const selectedCss =
+  'bg-primary-eexLight text-primary-pressing border-primary-main';
 
 interface ChipProps {
   children: ReactNode;
   disabled?: boolean;
+  value?: string;
+  checked?: boolean;
+  onClick?: (value: string) => void;
 }
 
-export default function WhChip({ children, disabled= false}: ChipProps) {
+export default function WhChip({
+  children,
+  disabled = false,
+  value = '',
+  checked = false,
+  onClick = () => {}, // set default function
+}: ChipProps) {
 
-  const [isSelected, setIsSelected] = useState(false);
-
-  const chipHandler = () => {
-    setIsSelected(!isSelected)
-  }
-  
   return (
-
     <button
       type='button'
       disabled={disabled}
-      onClick={chipHandler}
-      className={`
-      ${defaultChipCss}
-
-      ${isSelected ? isSelectedCss : 'hover:bg-nutral-white-02' }
-    `}
+      value={value}
+      onClick={() => onClick(value)}
+      className={`${defaultChipCss} ${checked ? selectedCss : 'hover:bg-nutral-white-02'}`}
     >
       {children}
     </button>
   );
 }
-
