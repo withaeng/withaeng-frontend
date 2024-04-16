26c9cf4ea6ff42ff721737388d70655cabe081e2
신수진 <shinsj5037@gmail.com>
2024-04-16 16:23:55 +0900

    fix: 회원가입, 이용약관 페이지 하나로 합침

---
+++ src/app/auth/@modal/(.)sign-up/component/signUpContent.tsx
@@ -0,0 +1,122 @@
+import WhModalHeader from '@/components/elements/modal/WhModalHeader';
+import WhRadio from '@/components/elements/WhRadio';
+import WhCheckbox from '@/components/elements/WhCheckbox';
+import WhInput from '@/components/elements/WhInput';
+import { useRouter } from 'next/navigation';
+import { UserSignUp } from '../page';
+import WhButton from '@/components/elements/WhButton';
+
+const secondarySpanCss = 'text-secondary-main text-subtitle-02';
+
+export default function SignUpContent({
+  form,
+  setForm,
+  setTermPage,
+}: {
+  form: UserSignUp;
+  setForm: React.Dispatch<React.SetStateAction<UserSignUp>>;
+  setTermPage: (value: boolean) => void;
+}) {
+  const router = useRouter();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!form.email) {
+      console.error('이메일을 입력하세요');
+      return;
+    }
+    if (!form.password) {
+      console.error('비밀번호를 입력하세요');
+      return;
+    }
+    if (!form.term) {
+      console.error('이용약관에 동의해주세요.');
+      return;
+    }
+    console.log(form);
+    router.replace('/auth/sign-up/email-check');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='flex flex-col h-full'>
+      <WhModalHeader>회원가입 정보 입력</WhModalHeader>
+      <div className='grow flex flex-col gap-10 mt-10 mx-[15px]'>
+        <WhInput
+          type='email'
+          label='이메일'
+          value={form.email}
+          handleInputChange={(value) =>
+            setForm((prev) => ({ ...prev, email: value }))
+          }
+          size='lg'
+          placeholder='이메일'
+        />
+        <WhInput
+          type='password'
+          label='비밀번호'
+          value={form.password}
+          handleInputChange={(value) =>
+            setForm((prev) => ({ ...prev, password: value }))
+          }
+          size='lg'
+          placeholder='비밀번호'
+        />
+        <div>생년월일 calendar 들어갈 자리</div>
+        <div>
+          <label htmlFor='sign-up_gender'>
+            성별
+            {/* FIXME: required label 처리 (임시 작업) */}
+            <span className='text-caption-main'>*</span>
+          </label>
+          <div id='sign-up_gender' className='flex mt-3 gap-8'>
+            <WhRadio
+              id='male'
+              name='gender'
+              value='male'
+              checked={form.gender === 'male'}
+              onChange={(value) =>
+                setForm((prev) => ({ ...prev, gender: value }))
+              }
+            >
+              남성
+            </WhRadio>
+            <WhRadio
+              id='female'
+              name='gender'
+              value='female'
+              checked={form.gender === 'female'}
+              onChange={(value) =>
+                setForm((prev) => ({ ...prev, gender: value }))
+              }
+            >
+              여성
+            </WhRadio>
+          </div>
+        </div>
+      </div>
+      <div className='flex justify-between mb-3'>
+        <label htmlFor='이용약관' className='text-body-03 text-nutral-black-05'>
+          <span className={secondarySpanCss}>만 14세 이용</span>에 동의하며,{' '}
+          <span
+            onClick={() => setTermPage(true)}
+            className={`${secondarySpanCss} underline cursor-pointer`}
+          >
+            개인정보 이용약관
+          </span>
+          에 동의합니다.
+        </label>
+        <WhCheckbox
+          id='이용약관'
+          value='term'
+          checked={form.term}
+          onChange={() => setForm((prev) => ({ ...prev, term: !prev.term }))}
+        >
+          {''}
+        </WhCheckbox>
+      </div>
+      <WhButton size='lg' type='submit'>
+        동의하며, 계속 진행하겠습니다.
+      </WhButton>
+    </form>
+  );
+}
--- src/app/auth/@modal/(.)sign-up/component/termContent.tsx
+++ src/app/auth/@modal/(.)sign-up/component/termContent.tsx
@@ -1,15 +1,24 @@
 'use client';
 
-import WhModal from '@/components/elements/modal/WhModal';
 import WhModalButtonList from '@/components/elements/modal/WhModalButtonList';
 import WhModalHeader from '@/components/elements/modal/WhModalHeader';
+import { UserSignUp } from '../page';
 
-export default function SignUpTermModal() {
-  const onClick = () => {};
+export default function TermContent({
+  setForm,
+  setTermPage,
+}: {
+  setForm: React.Dispatch<React.SetStateAction<UserSignUp>>;
+  setTermPage: (value: boolean) => void;
+}) {
+  const onClick = () => {
+    setForm((prev) => ({ ...prev, term: true }));
+    setTermPage(false);
+  };
   return (
-    <WhModal className='px-[105px] py-[72px]'>
+    <div className='flex flex-col h-full'>
       <WhModalHeader>이용약관</WhModalHeader>
-      <div className='mx-5 mt-5 mb-6 overflow-auto h-[470px]'>
+      <div className='grow mx-5 mt-5 mb-6 overflow-auto'>
         {`네이버는 본 개인정보처리방침은 개인정보보호법 기준으로 작성하되, 네이버 내에서의 이용자 
           개인정보 처리 현황을 최대한 알기 쉽고 상세하게 설명하기 위해 노력하였습니다. 이는 쉬운 용어를
           사용한 개인정보처리방침 작성 원칙인 'Plain Language Privacy Policy(쉬운 용어를 사용한 개인정보처리방침)’를 도입한 것입니다.
@@ -40,9 +49,10 @@ export default function SignUpTermModal() {
       </div>
       <WhModalButtonList
         prev
+        onPrevClick={() => setTermPage(false)}
         onClick={onClick}
         label='이용약관에 동의합니다.'
       />
-    </WhModal>
+    </div>
   );
 }
