bf7242f6f94e2573185f0d3de4b6398034d22c60
신수진 <shinsj5037@gmail.com>
2024-04-17 12:14:22 +0900

    fix: modal 컴포넌트 수정. route O / route X 모두 반영할 수 있도록

--- src/components/elements/modal/WhModal.tsx
+++ src/components/elements/modal/WhModal.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import { type ElementRef, useEffect, useRef } from 'react';
-import { useRouter } from 'next/navigation';
+import { useRef } from 'react';
 import { createPortal } from 'react-dom';
+import { CSSTransition } from 'react-transition-group';
 import { CloseIcon } from '../../../../public/assets/icons/menu';
 
 interface Props {
+  isOpen: boolean;
+  onClose: () => void;
   hideCloseButton?: boolean;
   isDismissible?: boolean;
   className?: string;
@@ -13,48 +15,46 @@ interface Props {
 }
 
 export default function WhModal({
+  isOpen,
+  onClose,
   hideCloseButton = false,
   isDismissible = true,
-  className = '',
+  className,
   children,
 }: Props) {
-  const router = useRouter();
-  const dialogRef = useRef<ElementRef<'dialog'>>(null);
-
-  useEffect(() => {
-    if (!dialogRef.current?.open) {
-      dialogRef.current?.showModal();
-    }
-  }, []);
-
-  function handleClose(closable: boolean) {
-    if (closable) router.back();
+  const nodeRef = useRef(null);
+  if (typeof window !== 'object') {
+    return null;
   }
 
   return createPortal(
-    <div className='flex justify-center items-center w-full h-full'>
-      <dialog
-        ref={dialogRef}
-        onClose={() => handleClose(true)}
-        onMouseDown={(e) =>
-          e.target === e.currentTarget && handleClose(isDismissible)
-        }
-        className='bg-nutral-white-01 z-50 w-fit min-w-[580px] rounded shadow-modal'
-      >
-        <div className={`w-[680px] ${className}`}>
-          {!hideCloseButton && (
-            <button
-              type='button'
-              aria-label='닫기 버튼'
-              onClick={() => handleClose(true)}
-            >
-              <CloseIcon className='absolute top-5 right-5' />
-            </button>
-          )}
-          {children}
+    <CSSTransition
+      in={isOpen}
+      timeout={300}
+      nodeRef={nodeRef}
+      mountOnEnter
+      unmountOnExit
+      classNames='modal-transition'
+      className='absolute top-0 left-0 w-dvw h-dvh'
+    >
+      <div ref={nodeRef}>
+        <div
+          role='presentation'
+          className='w-full h-full bg-[#000000] opacity-20 z-40'
+          onClick={isDismissible ? onClose : undefined}
+        />
+        <div className='modal-content absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-nutral-white-01 z-50 w-fit rounded shadow-modal'>
+          <div className={`w-[680px] ${className}`}>
+            {!hideCloseButton && (
+              <button type='button' aria-label='닫기 버튼' onClick={onClose}>
+                <CloseIcon className='absolute top-5 right-5' />
+              </button>
+            )}
+            {children}
+          </div>
         </div>
-      </dialog>
-    </div>,
+      </div>
+    </CSSTransition>,
     document.getElementById('modal-root')!
   );
 }
