fbad2c53c69eab6abbfbc43eab553eceee43268b
parksol <thfdl0317@naver.com>
2024-04-19 09:41:36 +0900

    fix : 뒷 배경 처리

--- src/app/mypage/layout.tsx
+++ src/app/mypage/layout.tsx
@@ -6,7 +6,7 @@ interface LayoutProps {
 
 export default function MypageLayout({ children }: LayoutProps) {
   return (
-    <div className='flex bg-nutral-white-02 h-screen gap-5 pt-[124px]'>
+    <div className='flex bg-nutral-white-02  gap-5 pt-[124px] overflow-auto'>
       {/* left */}
       <div className='w-[305px] flex items-end flex-col grow'>
         <div className='fixed h-dvh flex flex-col'>
@@ -15,7 +15,7 @@ export default function MypageLayout({ children }: LayoutProps) {
       </div>
 
       {/* right */}
-      <div className='flex items-start h-dvh flex-col grow'>
+      <div className='flex items-start h-dvh flex-col grow '>
         <main>{children}</main>
       </div>
     </div>
