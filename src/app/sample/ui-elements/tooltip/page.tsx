bda286ecc33e846b85e24503a969b298fd9335e1
parksol <thfdl0317@naver.com>
2024-05-08 13:17:52 +0900

    add : 툴팁아이콘, 스타일 추가 및 수정

--- src/app/sample/ui-elements/tooltip/page.tsx
+++ src/app/sample/ui-elements/tooltip/page.tsx
@@ -2,16 +2,16 @@ import WhTooltip from '@/components/elements/WhTooltip';
 
 export default function Page() {
   return (
-    <div className='flex gap-8 m-20'>
+    <div className='flex gap-40 m-20'>
       <WhTooltip position='top' coreText='동행' content='을 직접 찾아볼까요?'>
-        top일 경우 *(hover 시 노출)
+        top일 경우
       </WhTooltip>
       <WhTooltip
         position='bottom'
         coreText='동행'
         content='을 직접 찾아볼까요?'
       >
-        bottom일 경우 *(hover 시 노출)
+        bottom일 경우
       </WhTooltip>
     </div>
   );
--- src/components/elements/WhTooltip.tsx
+++ src/components/elements/WhTooltip.tsx
@@ -1,20 +1,26 @@
 'use client';
 
 import { useState } from 'react';
-import { DeleteFillIcon } from '../../../public/assets/icons/system';
+// import { DeleteFillIcon } from '../../../public/assets/icons/system';
+
+import { CloseIcon } from '../../../public/assets/icons/menu';
+import {
+  TooltipBottomArrowIcon,
+  TooltipTopArrowIcon,
+} from '../../../public/assets/icons/arrow';
 
 // const defaultTooltipCss =
 //   'absolute hidden group-hover:inline-block  whitespace-nowrap px-4 py-2 text-nutral-white-01 bg-secondary-main rounded';
 
 const defaultTooltipCss =
-  'absolute flex justify-center items-center gap-2 inline-block whitespace-nowrap px-4 py-2 text-nutral-white-01 bg-secondary-main rounded';
+  'absolute flex justify-center items-center inline-block whitespace-nowrap px-4 py-2 text-nutral-white-01 bg-secondary-main rounded';
 
 let positionCss = '';
 
 // const defaultPointCss =
 //   'absolute hidden group-hover:inline-block border-[10px]';
 
-const defaultPointCss = 'absolute inline-block border-[10px]';
+const defaultPointCss = 'absolute inline-block';
 let pointCss = '';
 
 interface TooltipProps {
@@ -31,35 +37,66 @@ export default function WhTooltip({
   children,
 }: TooltipProps) {
   if (position === 'top') {
-    positionCss = 'left-1/2 -translate-x-1/2 bottom-[calc(100%+5px)]';
+    positionCss = 'left-1/2 -translate-x-1/2 bottom-[calc(100%+12px)]';
   } else if (position === 'bottom') {
-    positionCss = 'left-1/2 -translate-x-1/2 top-[calc(100%+5px)]';
+    positionCss = 'left-1/2 -translate-x-1/2 top-[calc(100%+11px)]';
   }
 
+  // if (position === 'top') {
+  //   pointCss =
+  //     'left-3 bottom-full border-l-transparent border-r-transparent border-b-0 border-t-secondary-main';
+  // } else if (position === 'bottom') {
+  //   pointCss = 'right-5 border-t-0 border-b-secondary-main';
+  // }
+
   if (position === 'top') {
-    pointCss =
-      'left-1/2 -translate-x-1/2 bottom-full border-l-transparent border-r-transparent border-b-0 border-t-secondary-main';
+    pointCss = 'left-6 bottom-full';
   } else if (position === 'bottom') {
-    pointCss =
-      'left-1/2 -translate-x-1/2 top-full border-l-transparent border-r-transparent border-t-0 border-b-secondary-main';
+    pointCss = 'right-6';
   }
 
   const [isShow, setIsShow] = useState(true);
 
-  const handleClose = () => {
-    setIsShow(false);
+  const handleClick = () => {
+    setIsShow((prevState) => !prevState);
   };
 
   return (
-    <div className='relative cursor-pointer group'>
-      <div className='mx-2 my-1'>{children}</div>
-      <p className={`${defaultTooltipCss} ${positionCss}`}>
-        <span className='text-primary-main'>{coreText}</span>
-        {content}
-        <DeleteFillIcon onClick={handleClose} />
-      </p>
-
-      <span className={`${defaultPointCss} ${pointCss}`} />
+    <div>
+      <div className='relative group'>
+        <button
+          type='button'
+          onClick={handleClick}
+          className='mx-2 text-secondary-main cursor-pointer'
+        >
+          {children}
+        </button>
+        {isShow && (
+          <div>
+            <p className={`${defaultTooltipCss} ${positionCss}`}>
+              <span className='text-primary-main'>{coreText}</span>
+              {content}
+              <CloseIcon
+                onClick={handleClick}
+                className='ml-2 cursor-pointer'
+                stroke='#FAF8F6'
+                width={20}
+                height={20}
+              />
+            </p>
+            {/* <span className={`${defaultPointCss} ${pointCss}`} /> */}
+            {position === 'top' ? (
+              <span className={`${defaultPointCss} ${pointCss}`}>
+                <TooltipBottomArrowIcon width={14} height={14} />
+              </span>
+            ) : (
+              <span className={`${defaultPointCss} ${pointCss}`}>
+                <TooltipTopArrowIcon width={14} height={14} />
+              </span>
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
