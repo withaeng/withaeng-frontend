142c157e2d54439565b08aa2b835b1480a9c1f28
신수진 <shinsj5037@gmail.com>
2024-06-06 14:37:29 +0900

    ci: api 연동을 위한 header 및 서버 url config 설정

--- next.config.js
+++ next.config.js
@@ -26,6 +26,36 @@ const nextConfig = {
       },
     ],
   },
+
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination:
+          'http://ec2-52-79-248-251.ap-northeast-2.compute.amazonaws.com:8080/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
