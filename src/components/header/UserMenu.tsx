c96b439b3ad9d0b621b31315b85b2da744da5ac7
Sujin Shin <shinsj5037@gmail.com>
2024-08-22 13:05:43 +0900

    fix: 유저 메뉴 - 세션여부에 따라 보여주는 컴포넌트 전환 -  삼항연산자 -> 함수로 분리

--- src/components/header/UserMenu.tsx
+++ src/components/header/UserMenu.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { getCookie } from 'cookies-next';
+import { CookieValueTypes, getCookie } from 'cookies-next';
 import useAuth from '@/hooks/useAuth';
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import { useRouter } from 'next/navigation';
@@ -10,10 +10,72 @@ import { ProfilIconRed02 } from '../../../public/assets/icons/profile';
 const mypageGnbCss =
   '*:text-subtitle-01 py-2.5 px-5 hover:bg-primary-exLight w-full';
 
-export default function UserMenu() {
-  const session = getCookie('accessToken');
+function UserDropDownMenu({ session }: { session: CookieValueTypes }) {
   const { signout } = useAuth();
   const router = useRouter();
+
+  if (!session) {
+    return (
+      <>
+        <DropdownMenu.Label className='text-primary-pressing py-3 px-5'>
+          로그인이 필요합니다.
+        </DropdownMenu.Label>
+        <DropdownMenu.Item className={mypageGnbCss}>
+          <button
+            type='button'
+            onClick={() => router.push('/login')}
+            aria-label='로그인, 회원가입 버튼'
+          >
+            로그인 / 회원가입
+          </button>
+        </DropdownMenu.Item>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <DropdownMenu.Label className='text-primary-pressing py-3 px-5'>
+        같이행
+      </DropdownMenu.Label>
+      <DropdownMenu.Item className={mypageGnbCss}>
+        <button
+          type='button'
+          onClick={() => router.push('/mypage')}
+          aria-label='마이페이지 버튼'
+        >
+          마이페이지
+        </button>
+      </DropdownMenu.Item>
+      <DropdownMenu.Item className={mypageGnbCss}>
+        <button
+          type='button'
+          onClick={() => router.push('/mypage')}
+          aria-label='리뷰관리 버튼'
+        >
+          리뷰관리
+        </button>
+      </DropdownMenu.Item>
+      <DropdownMenu.Item className={mypageGnbCss}>
+        <button
+          type='button'
+          onClick={() => router.push('/mypage')}
+          aria-label='배지관리 버튼'
+        >
+          배지관리
+        </button>
+      </DropdownMenu.Item>
+      <DropdownMenu.Item className={mypageGnbCss}>
+        <button type='button' onClick={signout} aria-label='로그아웃 버튼'>
+          로그아웃
+        </button>
+      </DropdownMenu.Item>
+    </>
+  );
+}
+
+export default function UserMenu() {
+  const session: CookieValueTypes = getCookie('accessToken');
 
   return (
     <div className='xl:px-4 xl:py-3 xl:border rounded-full xl:border-nutral-white-03 relative'>
@@ -36,64 +98,7 @@ export default function UserMenu() {
             align='end'
             className='py-2 bg-nutral-white-02 shadow-modal w-[200px]'
           >
-            {session ? (
-              <>
-                <DropdownMenu.Label className='text-primary-pressing py-3 px-5'>
-                  같이행
-                </DropdownMenu.Label>
-                <DropdownMenu.Item className={mypageGnbCss}>
-                  <button
-                    type='button'
-                    onClick={() => router.push('/mypage')}
-                    aria-label='마이페이지 버튼'
-                  >
-                    마이페이지
-                  </button>
-                </DropdownMenu.Item>
-                <DropdownMenu.Item className={mypageGnbCss}>
-                  <button
-                    type='button'
-                    onClick={() => router.push('/mypage')}
-                    aria-label='리뷰관리 버튼'
-                  >
-                    리뷰관리
-                  </button>
-                </DropdownMenu.Item>
-                <DropdownMenu.Item className={mypageGnbCss}>
-                  <button
-                    type='button'
-                    onClick={() => router.push('/mypage')}
-                    aria-label='배지관리 버튼'
-                  >
-                    배지관리
-                  </button>
-                </DropdownMenu.Item>
-                <DropdownMenu.Item className={mypageGnbCss}>
-                  <button
-                    type='button'
-                    onClick={signout}
-                    aria-label='로그아웃 버튼'
-                  >
-                    로그아웃
-                  </button>
-                </DropdownMenu.Item>
-              </>
-            ) : (
-              <>
-                <DropdownMenu.Label className='text-primary-pressing py-3 px-5'>
-                  로그인이 필요합니다.
-                </DropdownMenu.Label>
-                <DropdownMenu.Item className={mypageGnbCss}>
-                  <button
-                    type='button'
-                    onClick={() => router.push('/login')}
-                    aria-label='로그인, 회원가입 버튼'
-                  >
-                    로그인 / 회원가입
-                  </button>
-                </DropdownMenu.Item>
-              </>
-            )}
+            <UserDropDownMenu session={session} />
             <DropdownMenu.Arrow className='fill-nutral-white-01' />
           </DropdownMenu.Content>
         </DropdownMenu.Portal>
