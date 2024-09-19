2f7f8a33dc441bd0e60431c67b3451dc9e1dffe2
신수진 <shinsj5037@gmail.com>
2024-09-19 13:41:44 +0900

    fix: 내 정보 관리 > 설정 > 탈퇴하기 : 모바일 뷰 수정

--- src/app/mypage/(component)/Withdraw.tsx
+++ src/app/mypage/(component)/Withdraw.tsx
@@ -12,7 +12,7 @@ export default function WithdrawModal() {
       <button
         type='button'
         onClick={onOpen}
-        className='text-secondary-exLight text-subtitle-02'
+        className='text-subtitle-02 text-secondary-exLight'
       >
         회원탈퇴
       </button>
@@ -20,23 +20,24 @@ export default function WithdrawModal() {
         isOpen={isOpen}
         onClose={onClose}
         isDismissible={false}
-        className='px-4 py-5 xl:px-[84px] xl:py-[72px] flex flex-col justify-between'
+        className='flex flex-col justify-between px-4 py-5 xl:px-[84px] xl:py-[72px]'
       >
         <div>
-
-        <WhModalHeader>
-          <span className='text-headline-04 text-primary-main'>같이행 </span>
-          서비스를 정말로 탈퇴하실건가요? 😥
-        </WhModalHeader>
-        <div className='mt-10 text-center mb-12'>
-          <p className='text-nutral-black-04  text-body-02'>
-            탈퇴하시면 회원님의 모든 정보와 활동 기록이
-            <br />
-            삭제돼요. 삭제된 정보는 복구하실 수 없습니다.
-            <br />
-            그래도 탈퇴하시는건가요?
-          </p>
-        </div>
+          <WhModalHeader>
+            <span className='text-subtitle-01 text-primary-main xl:text-headline-04'>
+              같이행{' '}
+            </span>
+            서비스를 정말로 탈퇴하실건가요? 😥
+          </WhModalHeader>
+          <div className='mb-12 mt-10 text-center'>
+            <p className='text-body-02 text-nutral-black-04'>
+              탈퇴하시면 회원님의 모든 정보와 활동 기록이
+              <br />
+              삭제돼요. 삭제된 정보는 복구하실 수 없습니다.
+              <br />
+              그래도 탈퇴하시는건가요?
+            </p>
+          </div>
         </div>
         <WhModalButtonList onClick={onClose} label='네, 탈퇴할래요.' />
       </WhModal>
