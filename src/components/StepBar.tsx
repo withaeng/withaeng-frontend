58a3b40e888cffac6a578b13113babb15263adc5
신수진 <shinsj5037@gmail.com>
2024-09-20 00:13:29 +0900

    fix: stepbar 모바일 뷰 반영

--- src/components/StepBar.tsx
+++ src/components/StepBar.tsx
@@ -1,6 +1,6 @@
 export default function StepBar({ value }: { value: number }): React.ReactNode {
   return (
-    <div className='h-0.5 w-full bg-primary-exLight mt-2'>
+    <div className='h-0.5 w-full bg-primary-exLight xl:mt-2'>
       <div
         className='h-full bg-primary-main transition-all'
         style={{ width: `${value}%` }}
