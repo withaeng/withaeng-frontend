94e589cddbc7d97716715b9dd8c7bc569c0958b1
parksol <thfdl0317@naver.com>
2024-09-22 02:35:13 +0900

    add : footer 반응형 작업완료

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,18 +5,22 @@ import {
 
 export default function Footer() {
   return (
-    <footer className='py-10 bg-nutral-white-02 border-t border-t-nutral-white-03'>
-      <ul className='flex justify-center items-center text-caption-02 text-nutral-white-04'>
-        <li className='flex items-center gap-2 text-caption-01 text-nutral-black-04 cursor-pointer px-5'>
+    <footer className='border-t border-t-nutral-white-03 bg-nutral-white-02 py-10'>
+      <ul className='flex cursor-pointer items-center justify-center gap-5 max-xl:flex-col'>
+        <li className='flex items-center gap-2 text-caption-01 text-nutral-black-04 max-xl:mb-[10px]'>
           <CopyrightIcon />
           2024 Withaeng
         </li>
-        <li>
+        <li className='max-xl:hidden'>
           <FooterLineIcon />
         </li>
-        <li className='cursor-pointer px-5'>위드행 이용약관</li>
-        <li className='cursor-pointer px-5'>개인정보 처리방침</li>
-        <li className='cursor-pointer px-5'>서비스 소개</li>
+        <li className='text-caption-02 text-nutral-white-04'>
+          위드행 이용약관
+        </li>
+        <li className='text-caption-02 text-nutral-white-04'>
+          개인정보 처리방침
+        </li>
+        <li className='text-caption-02 text-nutral-white-04'>서비스 소개</li>
       </ul>
     </footer>
   );
