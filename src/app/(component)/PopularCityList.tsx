a0fa61aa4410fe1e342ea9c40b403739fd3766a0
dev-damdam <dev.sodam@gmail.com>
2024-09-23 03:03:46 +0900

    feat: 인기 여행지 피그마 디자인 맞춰 추가 수정 작업

--- src/app/(component)/PopularCityList.tsx
+++ src/app/(component)/PopularCityList.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import Slider from 'react-slick';
+import Slider, { CustomArrowProps } from 'react-slick';
 import { TPopularCity } from '@/types/accompany';
 import PopularCityCard from '@/app/(component)/PopularCityCard';
 import Link from 'next/link';
@@ -11,33 +11,47 @@ import {
   ChevronLeftIcon,
   ChevronRightIcon,
 } from '../../../public/assets/icons/arrow';
+import '@/app/(component)/custom-react-slick.css';
 
 interface PopularCityListProps {
   list: TPopularCity[];
 }
 
-function NextArrow(props: any) {
-  const { onClick } = props;
+function NextArrow(props: CustomArrowProps) {
+  const { onClick, currentSlide } = props;
+  const hidden = currentSlide === 3 ? 'hidden' : 'inline-block'; // 하드코딩
   return (
     <button
       onClick={onClick}
-      className='absolute right-1 top-[50%] z-10 -translate-y-2/4 rounded-full border border-nutral-white-03 bg-nutral-white-01 p-2 max-xl:hidden'
+      className={`${hidden} group absolute right-1 top-[50%] z-10 -translate-y-2/4 rounded-full border border-nutral-white-03 bg-nutral-white-01 p-2 hover:border-primary-main max-xl:hidden`}
       aria-label='다음 버튼'
     >
-      <ChevronRightIcon width={24} height={24} stroke='#737373' />
+      <ChevronRightIcon
+        width={24}
+        height={24}
+        stroke='#737373'
+        className='group-hover:stroke-primary-main'
+      />
     </button>
   );
 }
 
-function PrevArrow(props: any) {
-  const { onClick } = props;
+function PrevArrow(props: CustomArrowProps) {
+  const { onClick, currentSlide } = props;
+  const hidden = currentSlide === 0 ? 'hidden' : 'inline-block';
+
   return (
     <button
       onClick={onClick}
-      className='absolute left-1 top-[50%] z-10 -translate-y-2/4 rounded-full border border-nutral-white-03 bg-nutral-white-01 p-2 max-xl:hidden'
+      className={`${hidden} group absolute left-1 top-[50%] z-10 -translate-y-2/4 rounded-full border border-nutral-white-03 bg-nutral-white-01 p-2 hover:border-primary-main max-xl:hidden`}
       aria-label='이전 버튼'
     >
-      <ChevronLeftIcon width={24} height={24} stroke='#737373' />
+      <ChevronLeftIcon
+        width={24}
+        height={24}
+        stroke='#737373'
+        className='group-hover:stroke-primary-main'
+      />
     </button>
   );
 }
@@ -50,7 +64,7 @@ export default function PopularCityList({ list }: PopularCityListProps) {
   };
 
   const settings = {
-    slidesToShow: 4,
+    slidesToShow: 5,
     slidesToScroll: 5,
     variableWidth: true,
     infinite: false,
@@ -59,6 +73,15 @@ export default function PopularCityList({ list }: PopularCityListProps) {
     afterChange: handleAfterChange,
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
@@ -66,9 +89,7 @@ export default function PopularCityList({ list }: PopularCityListProps) {
       <Slider {...settings}>
         {list?.map((item) => (
           <Link href={`/popular-city/${item.cityEng}`} key={item.id}>
-            <div className='pr-5'>
-              <PopularCityCard item={item} />
-            </div>
+            <PopularCityCard item={item} />
           </Link>
         ))}
       </Slider>
