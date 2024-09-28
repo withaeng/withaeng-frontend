3bc93f8e4237837560b81f301e9bc25a4b802680
신수진 <shinsj5037@gmail.com>
2024-09-28 09:36:36 +0900

    fix: next rewrite 설정 수정

--- next.config.js
+++ next.config.js
@@ -50,8 +50,8 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/:path*',
-        destination: process.env.API_BASE_URL + '/:path*',
+        source: '/api/:path*',
+        destination: process.env.API_BASE_URL + '/api/:path*',
       },
     ];
   },
