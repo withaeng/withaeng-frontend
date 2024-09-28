441880279b80e9a05bf315705995db3ec184e21a
parksol <thfdl0317@naver.com>
2024-09-28 11:57:53 +0900

    fix : AccompanyData 타입 수정

--- src/types/accompany.ts
+++ src/types/accompany.ts
@@ -61,3 +61,19 @@ export type TAccompanySearch = {
   maxAllowedAge: AccompanyAge;
   preferGender: AccompanyGender;
 };
+
+export type AccompanyData = {
+  title: string;
+  content: string;
+  continent: string;
+  country: string;
+  city: string;
+  startTripDate: Date;
+  endTripDate: Date;
+  bannerImageUrl: string;
+  accompanyCnt: number;
+  openKakaoUrl: string;
+  age: number | number[];
+  gender: string;
+  tags: string[];
+};
