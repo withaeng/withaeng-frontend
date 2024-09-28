6d98bbe1a5ba78b5c1bb398ed4480407b83a55f5
parksol <thfdl0317@naver.com>
2024-09-28 10:32:15 +0900

    fix : 이전 footer 되돌림

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,21 +6,17 @@ import {
 export default function Footer() {
   return (
     <footer className='border-t border-t-nutral-white-03 bg-nutral-white-02 py-10'>
-      <ul className='flex cursor-pointer items-center justify-center gap-5 max-xl:flex-col'>
-        <li className='flex items-center gap-2 text-caption-01 text-nutral-black-04 max-xl:mb-[10px]'>
+      <ul className='flex items-center justify-center text-caption-02 text-nutral-white-04'>
+        <li className='flex cursor-pointer items-center gap-2 px-5 text-caption-01 text-nutral-black-04'>
           <CopyrightIcon />
           2024 Withaeng
         </li>
-        <li className='max-xl:hidden'>
+        <li>
           <FooterLineIcon />
         </li>
-        <li className='text-caption-02 text-nutral-white-04'>
-          위드행 이용약관
-        </li>
-        <li className='text-caption-02 text-nutral-white-04'>
-          개인정보 처리방침
-        </li>
-        <li className='text-caption-02 text-nutral-white-04'>서비스 소개</li>
+        <li className='cursor-pointer px-5'>위드행 이용약관</li>
+        <li className='cursor-pointer px-5'>개인정보 처리방침</li>
+        <li className='cursor-pointer px-5'>서비스 소개</li>
       </ul>
     </footer>
   );
