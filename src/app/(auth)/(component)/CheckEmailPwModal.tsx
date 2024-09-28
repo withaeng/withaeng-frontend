d89fe251eab2be7da5ee1d60fa8566bb24a004b6
Sujin Shin <67135103+ssj5037@users.noreply.github.com>
2024-09-28 10:21:49 +0900

    fix: 로그인/회원가입 api 작업 완료 (#72)
    * fix: 회원가입, 로그인, 로그아웃 - 백엔드에 맞춰 수정. url dash style로 수정.
    
    * fix: 선호등록 - 백엔드에 맞춰 수정 진행 중.
    
    * fix: 이메일 재전송, 비밀번호 변경 - 백엔드에 맞춰 수정
    
    * fix: 비밀번호 변경, 메일 재전송 에러 수정
    
    * fix: 선호 등록 에러 수정중.

--- src/app/(auth)/(component)/CheckEmailPwModal.tsx
+++ src/app/(auth)/(component)/CheckEmailPwModal.tsx
@@ -1,20 +1,33 @@
 'use client';
 
-import { useSearchParams } from 'next/navigation';
+import { useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
 import WhModal from '@/components/elements/modal/WhModal';
 import CheckEmailModalComponent from '@/app/(auth)/(component)/components/sign-up/CheckEmailModalContent';
 import useAuth from '@/hooks/useAuth';
 
 export default function CheckEmailPwModalPage() {
+  const router = useRouter();
   const { sendEmailPw } = useAuth();
-  const param = useSearchParams();
-  console.log(param.get('email'));
+  const searchParams = useSearchParams();
+  console.log(searchParams.get('email'));
+  // FIXME: (임시 작업) validate email api를 다녀왔는지 확인하기 위함. 아니면 무한 렌더링에 빠져버림...
+  const [isChecked, setIsChecked] = useState(false);
+
+  if (!isChecked && searchParams.has('email') && searchParams.has('code')) {
+    const email = searchParams.get('email') ?? '';
+    const code = searchParams.get('code') ?? '';
+    setIsChecked(true);
+    router.replace(`/change-password?email=${email}&code=${code}`);
+  }
 
   const handleEmail = async () => {
     console.log('이메일 재전송');
-    await sendEmailPw.mutateAsync({ email: param.get('email')! }).then(() => {
-      console.log('이메일 재전송이 완료되었습니다.');
-    });
+    await sendEmailPw
+      .mutateAsync({ email: searchParams.get('email')! })
+      .then(() => {
+        console.log('이메일 재전송이 완료되었습니다.');
+      });
   };
 
   return (
---
+++ src/app/@modal/(auth)/(.)complete-password/page.tsx
@@ -0,0 +1,5 @@
+import CompleteChangePasswordModalPage from '@/app/(auth)/(component)/CompleteChangePasswordModal';
+
+export default function CompletePasswordPage() {
+  return <CompleteChangePasswordModalPage />;
+}
